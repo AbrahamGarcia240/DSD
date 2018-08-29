a3f1bbd6a6cd0d71cb7593b6b1ec41c9e228117f
AbrahamGarcia240 <zildjianremo@gmail.com>
2018-08-29 12:34:42 -0500

    Ya imprime todas las cadenas

--- Practica7/ejercicio7-3-1.cpp
+++ Practica7/ejercicio7-3-1.cpp
@@ -7,17 +7,39 @@ using namespace std;
 int main(){
 	char *grande = (char*)malloc(sizeof(char));
 	
-	char *cad;
+	char *cad=(char*)malloc(sizeof(char)*5);
+
 	srand(time(NULL));
-	int i;
-	for(i=0;i<100;i++){
+	int i=1;
+	int k=1;
+	for(i=1;i<=100;i++){
+		//cout<<i<<endl;
+		cout<<endl;
 		for(int j=0;j<3;j++){
 			cad[j]=(rand()%25)+65;
+			//cout<<cad[j];
+			k+=1;
 		}
-		cout<<i<<endl;
-		cad[4]=' ';
-		grande = (char*)realloc(grande,sizeof(char)*4);
-		memcpy(grande,cad,4);
+		
+		cad[3]=' ';
+		cad[4]='\0';
+		puts(cad);
+		grande=(char*) realloc(grande,strlen(grande)+strlen(cad)+1);
+		strcat(grande,cad);
+		//memcpy(grande,cad,strlen(cad)+1);
+		
+
+
+
+
+		
 	}
-	//cout<<grande<<endl;
+	cout<<endl;
+	cout<<k<<endl;
+	cout<<"CADENOTA:"<<endl;
+	cout<<sizeof(grande)<<endl;
+	cout<<grande<<endl;
+
+	free(grande);
+	free(cad);
 }
