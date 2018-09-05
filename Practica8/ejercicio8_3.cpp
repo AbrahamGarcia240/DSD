58e8c616c710a8ecae6a7657b81c4cc94c49b7aa
Francisco Javier Lucio Silva <btbammyung@gmail.com>
2018-09-05 10:21:09 -0500

    correccion 8.3

--- Practica8/ejercicio8_3.cpp
+++ Practica8/ejercicio8_3.cpp
@@ -40,19 +40,11 @@ return (longitud * Circulo::calcval());
 
 
 //4*pi² * R * r
-class Toroide : public Cilindro, public Circulo{
-private: 
-	double Radio, radio;
-public:
-Toroide(double r, double l, double R, double p): Cilindro(r,l), Radio(R), radio(p){}
-double calcval();
+class Toroide : public Cilindro{
 
 };
 
 
-double Toroide::calcval(){
-return 4*PI*PI*Radio*radio;
-}
 
 
 
@@ -63,12 +55,11 @@ int main()
 {
 Circulo Circulo_1, Circulo_2(2);
 Cilindro Cilindro_1(3,4);
-Toroide toro(1,2,3,4);
+Toroide toro;
 cout << "El área de Circulo_1 es " << Circulo_1.calcval() << endl;
 cout << "El área de Circulo_2 es " << Circulo_2.calcval() << endl;
 cout << "El volumen de Cilindro_1 es " << Cilindro_1.calcval() << endl;
-cout << "El area del toroide es" << toro.calcval()<<endl;
-
+cout << "Algo de cilindro es" << toro.calcval() <<endl;
 Circulo_1 = Cilindro_1;
 
 cout << "\nEl área de Circulo_1 es ahora " << Circulo_1.calcval() << endl;
